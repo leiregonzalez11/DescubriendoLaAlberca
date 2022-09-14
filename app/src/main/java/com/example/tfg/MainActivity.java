f995298426f250ad24743b4c6d40b32f739ead98
Leire Gonzalez <leiregonzalezlopez99@gmail.com>
2022-09-14 19:55:32 +0200

    Avances Splash Screen

--- app/src/main/java/com/example/tfg/MainActivity.java
+++ app/src/main/java/com/example/tfg/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.tfg;
 
+import static java.lang.Thread.sleep;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
