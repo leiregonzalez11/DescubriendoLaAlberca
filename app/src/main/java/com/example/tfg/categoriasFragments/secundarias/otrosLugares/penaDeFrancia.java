52c05c412d082dba06acbf7d864ec21fb006bdb5
Leire Gonzalez <leiregonzalezlopez99@gmail.com>
2022-12-09 15:37:29 +0100

    IU_PeñaDeFrancia creada y lista para implementación
    - Recetas:
    ------- INTRO: 7/10 añadidas
    ------- ES: 10/10 añadidas
    ------- EU: 10/10 añadidas
    ------- EN: 10/10 añadidas

---
+++ app/src/main/java/com/example/tfg/categoriasFragments/secundarias/otrosLugares/penaDeFrancia.java
@@ -0,0 +1,194 @@
+package com.example.tfg.categoriasFragments.secundarias.otrosLugares;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.text.HtmlCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.example.tfg.GestorDB;
+import com.example.tfg.R;
+import com.example.tfg.adapters.SpinnerAdapter;
+import com.example.tfg.adapters.listViewAdapter;
+import com.example.tfg.categoriasFragments.principal.otrosLugaresInicio;
+import com.example.tfg.categoriasFragments.secundarias.gastronomia.recetasTipicas;
+import com.example.tfg.categoriasFragments.secundarias.gastronomia.turroneras;
+import com.example.tfg.navigationmenu.Categorias;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link penaDeFrancia#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class penaDeFrancia extends Fragment {
+
+    private Bundle args;
+    private double lat, lon;
+    private Fragment fragment;
+    private TextView text1, text2, text3, text4, text5;
+    private ListView listView, listView2;
+    private SupportMapFragment mapFragment;
+    private String idioma, categoria, opc1, opc2;
+
+    /** Este callback se activa cuando el mapa está listo para ser utilizado. */
+    private final OnMapReadyCallback callback = new OnMapReadyCallback() {
+        /**
+         * Manipula el mapa una vez haya sido creado.
+         * Aquí es donde podemos añadir marcadores o líneas, añadir listeners o mover la cámara.
+         */
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            //Localización de la peña de francia
+            LatLng location = new LatLng(lat, lon);
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location, 15.5f));
+            //Tipo de mapa: Hibrido
+            googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        }
+    };
+
+    /**
+     * Utilizaremos este Factory Method para crear una nueva instancia
+     * de este fragmento utilizando los parámetros dados.
+     * @return Una nueva instancia del Fragment.
+     */
+    public static penaDeFrancia newInstance(Bundle args) {
+        penaDeFrancia fragment = new penaDeFrancia();
+        if (args != null){
+            fragment.setArguments(args);
+        }
+        return fragment;
+    }
+
+    /** Required empty public constructor */
+    public penaDeFrancia() {}
+
+    /** El Fragment ha sido creado.
+     * Aqui fijamos los parámetros que tengan que ver con el Activity. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(false);
+
+        args = new Bundle();
+
+        if (getArguments() != null) {
+            idioma = getArguments().getString("idioma");
+            categoria = getArguments().getString("categoria");
+        }
+
+        args.putString("idioma", idioma);
+        args.putString("categoria", categoria);
+
+        Toolbar myToolbar = requireActivity().findViewById(R.id.toolbar);
+        myToolbar.setNavigationIcon(R.drawable.ic_circle_arrow_left_solid);
+        myToolbar.setNavigationOnClickListener(view1 -> {
+            myToolbar.setNavigationIcon(null);
+            Fragment fragment = otrosLugaresInicio.newInstance(args);
+            cargarFragment(fragment);
+        });
+    }
+
+    /** El Fragment va a cargar su layout, el cual debemos especificar.
+     Aquí se instanciarán los objetos que si son vistas */
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_pena_de_francia, container, false);
+        if (v != null){
+            text1 = v.findViewById(R.id.pena11);
+            text2 = v.findViewById(R.id.pena12);
+            text3 = v.findViewById(R.id.pena13);
+            text4 = v.findViewById(R.id.pena14);
+            text5 = v.findViewById(R.id.pena15);
+            listView = v.findViewById(R.id.listviewPena1);
+            listView2 = v.findViewById(R.id.listviewPena2);
+        }
+        return v;
+    }
+
+    /** La vista de layout ha sido creada y ya está disponible
+     Aquí fijaremos todos los parámetros de nuestras vistas **/
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        GestorDB dbHelper = new GestorDB(getContext());
+
+        String[] datos = dbHelper.obtenerDescrGastro(idioma, "inicio", categoria, 2);
+
+        text1.setText(datos[0] + HtmlCompat.fromHtml("<br>", HtmlCompat.FROM_HTML_MODE_LEGACY));
+        text2.setText(datos[1] + HtmlCompat.fromHtml("<br>", HtmlCompat.FROM_HTML_MODE_LEGACY));
+        text3.setText(datos[2] + HtmlCompat.fromHtml("<br>", HtmlCompat.FROM_HTML_MODE_LEGACY));
+        text4.setText(datos[3] + HtmlCompat.fromHtml("<br>", HtmlCompat.FROM_HTML_MODE_LEGACY));
+        text5.setText(datos[4] + HtmlCompat.fromHtml("<br>", HtmlCompat.FROM_HTML_MODE_LEGACY));
+
+        /*----------
+         | Historia |
+         ----------*/
+
+        opc1 = getString(R.string.historiamayus);
+
+        ArrayList<String> lista1 = new ArrayList<>();
+        lista1.add(opc1);
+
+        listViewAdapter myAdapter = new listViewAdapter(getContext(), R.layout.list_item, lista1);
+        listView.setAdapter(myAdapter);
+
+        listView.setOnItemClickListener((adapterView, v, position, id) -> {
+            //fragment = turroneras.newInstance(args);
+            cargarFragment(fragment);
+        });
+
+        /*---------------
+         | ¿Qué visitar? |
+         ---------------*/
+
+        opc2 = "¿Qué visitar?";
+
+        ArrayList<String> lista2 = new ArrayList<>();
+        lista2.add(opc2);
+
+        listViewAdapter myAdapter2 = new listViewAdapter(getContext(), R.layout.list_item, lista2);
+        listView2.setAdapter(myAdapter2);
+
+        listView2.setOnItemClickListener((adapterView, v, position, id) -> {
+            //fragment = recetasTipicas.newInstance(args);
+            cargarFragment(fragment);
+        });
+
+    }
+
+    private void cargarFragment(Fragment fragment){
+        // Obtenemos el administrador de fragmentos a través de la actividad
+        FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+        // Definimos una transacción
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        // Remplazamos el contenido principal por el fragmento
+        fragmentTransaction.replace(R.id.relativelayout, fragment);
+        fragmentTransaction.addToBackStack(null);
+        // Cambiamos el fragment en la interfaz
+        fragmentTransaction.commit();
+    }
+}
