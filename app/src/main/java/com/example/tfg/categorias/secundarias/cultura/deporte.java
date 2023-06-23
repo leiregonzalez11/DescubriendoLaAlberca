fc2cf0747d52cbe28c52f246e683747cfbd7cb16
Leire Gonzalez <leiregonzalezlopez99@gmail.com>
2023-06-23 16:49:40 +0200

    Cultura: Deporte :heavy_check_mark:

--- app/src/main/java/com/example/tfg/categorias/secundarias/cultura/deporte.java
+++ app/src/main/java/com/example/tfg/categorias/secundarias/cultura/deporte.java
@@ -1,6 +1,8 @@
 package com.example.tfg.categorias.secundarias.cultura;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -14,13 +16,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.tfg.R;
-import com.example.tfg.categorias.secundarias.cultura.diccionario.diccionario;
 import com.example.tfg.navigationMenu.Categorias;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -32,6 +36,8 @@ public class deporte extends Fragment {
     private Bundle args;
     private String idioma;
     private Button btntresvalles, btntirolinas;
+    private ImageView img1, img2, img3;
+    private StorageReference storageRef;
 
     /**
      * Utilizaremos este Factory Method para crear una nueva instancia
@@ -85,6 +91,9 @@ public class deporte extends Fragment {
         if (v != null){
             btntresvalles = v.findViewById(R.id.tresvalles);
             btntirolinas = v.findViewById(R.id.tirolinas);
+            img1 = v.findViewById(R.id.imgdeporte1);
+            img2 = v.findViewById(R.id.imgdeporte2);
+            img3 = v.findViewById(R.id.imgdeporte3);
         }
         return v;
     }
@@ -96,13 +105,29 @@ public class deporte extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 
         btntresvalles.setOnClickListener(v -> {
-            Toast.makeText(getContext(), "Has Pulsado Tres Valles", Toast.LENGTH_SHORT).show();
+            Uri uri = Uri.parse("https://www.tresvalles.es/"); // Creamos una uri con la web
+            Intent dial = new Intent(Intent.ACTION_VIEW, uri); // Creamos una llamada al Intent de llamadas
+            startActivity(dial); // Ejecutamos el Intent
         });
 
         btntirolinas.setOnClickListener(v -> {
-            Toast.makeText(getContext(), "Has Pulsado Tirolinas", Toast.LENGTH_SHORT).show();
+            Uri uri = Uri.parse("https://www.gredostormes.com/parque-la-alberca/"); // Creamos una uri con la web
+            Intent dial = new Intent(Intent.ACTION_VIEW, uri); // Creamos una llamada al Intent de llamadas
+            startActivity(dial); // Ejecutamos el Intent
         });
 
+        storageRef = FirebaseStorage.getInstance().getReference();
+
+        obtenerImagenFirebase("categorias/cultura/vueltaciclistalaalberca.png", img1);
+        obtenerImagenFirebase("categorias/cultura/piraguaslaalberca.png", img2);
+        obtenerImagenFirebase("categorias/cultura/bolosdeporte.png", img3);
+
+    }
+
+    /** Método utilizado para obtener la imagen de Firebase Storage */
+    private void obtenerImagenFirebase(String path, ImageView img){
+        StorageReference pathReference = storageRef.child(path);
+        pathReference.getDownloadUrl().addOnSuccessListener(uri -> Glide.with(requireContext()).load(uri).into(img));
     }
 
     private void cargarFragment(Fragment fragment){
